0cfc087cd6c256e3a46c4544619efe2514140760
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2025-02-06 03:37:59 -0600

    logind user data show dashboard page

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,13 +1,35 @@
 import { authOptions } from "@/utils/authOptions";
 import { getServerSession } from "next-auth";
+import Image from "next/image";
 
 const DashboardPage = async () => {
   const session = await getServerSession(authOptions);
-  console.log(session);
 
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+      {session ? (
+        <div>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome To {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            User Email:{session?.user?.email}
+          </h1>
+          <div className="flex justify-center mt-10">
+            <Image
+              src={session?.user?.image ?? "/default-image.png"}
+              width={100}
+              height={100}
+              alt="User Image"
+              className="mx-auto rounded-full"
+            />
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1 className="text-4xl text-center mt-10">You are not logged in</h1>
+        </div>
+      )}
     </div>
   );
 };
